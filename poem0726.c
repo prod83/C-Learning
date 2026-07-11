0eb6fa40c05eb604c5ec2904d2eb4d09ed0875da
prod83 <peterc93rodriguez@gmail.com>
2026-07-10 20:10:21 -0700

    First 2
    entering the C world and a short poem

---
+++ poem0726.c
@@ -0,0 +1,16 @@
+
+
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("I am a man, \n");
+	printf("I am a tiger, \n");
+	printf("I am a poet, \n");
+	printf("I am a student,\n");
+	printf("I am a man.\n");
+	
+	return 0;
+}
+
